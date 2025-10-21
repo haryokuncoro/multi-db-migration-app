2613bf8a55392b0fb53561f4e04fc4176a38794e
haryo <emadekuncoro@gmail.com>
2025-10-21 12:53:36 +0700

    initial commit

---
+++ src/main/java/com/project/migration_runner/MigrationRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.project.migration_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/migration_runner/MigrationRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.migration_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
